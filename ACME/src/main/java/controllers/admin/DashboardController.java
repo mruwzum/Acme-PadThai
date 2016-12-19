0bbad47156c8b9dd32a1543ff97ed2bb2744ec67
Miguel Ángel Antolin Bermúdez <chicoantolin@gmail.com>
2016-12-19 23:27:30 +0100

    dashboard II

--- ACME/src/main/java/controllers/admin/DashboardController.java
+++ ACME/src/main/java/controllers/admin/DashboardController.java
@@ -3,6 +3,7 @@ package controllers.admin;
 import controllers.AbstractController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by mruwzum on 19/12/16.
@@ -14,6 +15,20 @@ public class DashboardController extends AbstractController {
     public DashboardController(){super();}
 
 
+    @RequestMapping(value = "/dashboard")
+    public ModelAndView dashboard(){
+        ModelAndView res;
+
+
+
+
+
+        res = new ModelAndView("admin/dashboard");
+
+
+
+        return res;
+    }
 
 
 }
