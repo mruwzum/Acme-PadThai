a3d47d4d07562f7c627b634a50a329b0f6994c58
Miguel Ángel Antolin Bermúdez <chicoantolin@gmail.com>
2016-12-19 23:22:17 +0100

    dashboard

---
+++ ACME/src/main/java/controllers/admin/DashboardController.java
@@ -0,0 +1,19 @@
+package controllers.admin;
+
+import controllers.AbstractController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by mruwzum on 19/12/16.
+ */
+@Controller
+@RequestMapping("admin")
+public class DashboardController extends AbstractController {
+
+    public DashboardController(){super();}
+
+
+
+
+}
