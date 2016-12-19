5292c3c9814d616069ae5ada17bb3750cef38829
Miguel Ángel Antolin Bermúdez <chicoantolin@gmail.com>
2016-12-19 18:10:53 +0100

    34.1

--- ACME/src/main/java/controllers/admin/UtilitiesController.java
+++ ACME/src/main/java/controllers/admin/UtilitiesController.java
@@ -1,6 +1,5 @@
 package controllers.admin;
 
-import com.sun.javafx.sg.PGShape;
 import domain.Campaing;
 import domain.Contest;
 import domain.MasterClass;
@@ -10,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import services.*;
@@ -111,6 +111,27 @@ public class UtilitiesController {
         actorService.editPersonalData(name,surname,emailAddress,phone,postalAddress, null);
         res = new ModelAndView("redirect:http://localhost:8080");
         return res;
+    }
+
+
+
+    @RequestMapping(value = "/banner/editCost")
+    public ModelAndView editBannerCost(@RequestParam int campID){
+        ModelAndView res;
+        Campaing aux = campaingService.findOne(campID);
+        adminService.setBannerCost(aux,0.25);
+        res = new ModelAndView("redirect:http://localhost:8080");
+        return res;
+    }
+    @RequestMapping(value = "/campaing/listAll")
+    public ModelAndView list() {
+        ModelAndView result;
+        Collection<Campaing> aux = campaingService.findAll();
+        result = new ModelAndView("campaign/list");
+        result.addObject("campaign", aux);
+        result.addObject("requestURI", "admin/campaing/listAll.do");
+        return result;
+
     }
 
 }
