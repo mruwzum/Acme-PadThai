347dee127b99f38759c5a85f5af6a7858336c085
Miguel Ángel Antolin Bermúdez <chicoantolin@gmail.com>
2016-12-19 17:48:30 +0100

    f

---
+++ ACME/src/main/java/controllers/admin/EditBannerCostController.java
@@ -0,0 +1,9 @@
+package controllers.admin;
+
+import controllers.AbstractController;
+
+/**
+ * Created by mruwzum on 19/12/16.
+ */
+public class EditBannerCostController extends AbstractController {
+}
