4423cb72f0df981e49e2266e2b9d8fc2c4b772c4
Miguel Ángel Antolin Bermúdez <chicoantolin@gmail.com>
2016-12-21 13:56:28 +0100

    test

--- ACME/src/main/java/controllers/actor/CommentController.java
+++ ACME/src/main/java/controllers/actor/CommentController.java
@@ -44,11 +44,10 @@ public class CommentController extends AbstractController {
 
 
     @RequestMapping(value = "/comment/write")
-    public ModelAndView write(@RequestParam int id, String title, String text, String stars){
+    public ModelAndView write(@RequestParam String title, String text, String stars){
         ModelAndView res;
 
-        res = new ModelAndView("mensaje/text");
-        res.addObject("id", id);
+        res = new ModelAndView("comment/write");
         res.addObject("title", title);
         res.addObject("text", text);
         res.addObject("stars", stars);
